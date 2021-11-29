mgaido91/FBK-fairseq-ST

[DUALDEC] Initial implementation of DirMul model

examples/speech_recognition/criterions/cross_entropy_dualdecoder.py
@@ -1,0 +1,79 @@
+import math
+
+
+from fairseq import utils, metrics
+from fairseq.criterions import FairseqCriterion, register_criterion
+from fairseq.criterions.label_smoothed_cross_entropy import label_smoothed_nll_loss
+
+
+@register_criterion("cross_entropy_dualdecoder")
+class CrossEntropyMultitask(FairseqCriterion):
+    def __init__(self, args, task):
+        super().__init__(task)
+        self.eps = args.label_smoothing
+        self.auxiliary_loss_weight = args.auxiliary_loss_weight
+        self.primary_loss_weight = args.primary_loss_weight
+
+    @classmethod
+    def build_criterion(cls, args, task):
+        return cls(args, task)
+
+    @staticmethod
+    def add_args(parser):
+        parser.add_argument('--primary-loss-weight', default=1.0, type=float, metavar='W',
+                            help='The weight to apply to the primary loss function when summing losses')
+        parser.add_argument('--auxiliary-loss-weight', default=1.0, type=float, metavar='W',
+                            help='The weight to apply to the auxiliary loss function when summing losses')
+        parser.add_argument('--label-smoothing', default=0., type=float, metavar='D',
+                            help='epsilon for label smoothing, 0 means no label smoothing')
+
+    def forward(self, model, sample, reduce=True, log_probs=True):
+        net_output = model(**sample['net_input'])
+        sample_size = sample['target'].size(0) if self.sentence_avg else sample['ntokens']
+        lprobs = model.get_normalized_probs(net_output[0], log_probs=True)
+        lprobs = lprobs.view(-1, lprobs.size(-1))
+        target = model.get_targets(sample, net_output[0]).view(-1, 1)
+        primary_loss, primary_nll_loss = label_smoothed_nll_loss(
+            lprobs, target, self.eps, ignore_index=self.padding_idx, reduce=reduce,
+        )
+        auxiliary_probs = model.auxiliary_decoder.get_normalized_probs(net_output[1], log_probs=True)
+        auxiliary_probs = auxiliary_probs.view(-1, auxiliary_probs.size(-1))
+        auxiliary_target = model.get_auxiliary_target(sample, net_output[1]).view(-1, 1)
+        auxiliary_loss, auxiliary_nll_loss = label_smoothed_nll_loss(
+            auxiliary_probs, auxiliary_target, self.eps, ignore_index=self.padding_idx, reduce=reduce,
+        )
+        loss = self.primary_loss_weight * primary_loss + self.auxiliary_loss_weight * auxiliary_loss
+        logging_output = {
+            'loss': loss.data,
+            'primary_loss': primary_loss.data,
+            'primary_nll_loss': primary_nll_loss.data,
+            'auxiliary_loss': auxiliary_loss.data,
+            'auxiliary_nll_loss': auxiliary_nll_loss.data,
+            'ntokens': sample['ntokens'],
+            'nsentences': sample['target'].size(0),
+            'sample_size': sample_size,
+        }
+        return loss, sample_size, logging_output
+
+    @staticmethod
+    def logging_outputs_can_be_summed():
+        return True
+
+    @staticmethod
+    def reduce_metrics(logging_outputs) -> None:
+        """Aggregate logging outputs from data parallel training."""
+        loss_sum = sum(log.get('loss', 0) for log in logging_outputs)
+        primary_loss_sum = sum(log.get('primary_loss', 0) for log in logging_outputs)
+        auxiliary_loss_sum = sum(log.get('auxiliary_loss', 0) for log in logging_outputs)
+        primary_nll_loss = sum(log.get('primary_nll_loss', 0) for log in logging_outputs)
+        auxiliary_nll_loss = sum(log.get('auxiliary_nll_loss', 0) for log in logging_outputs)
+        ntokens = sum(log.get('ntokens', 0) for log in logging_outputs)
+        sample_size = sum(log.get('sample_size', 0) for log in logging_outputs)
+
+        metrics.log_scalar('loss', loss_sum / sample_size / math.log(2), sample_size, round=3)
+        metrics.log_scalar('primary_loss', primary_loss_sum / sample_size / math.log(2), sample_size, round=3)
+        metrics.log_scalar('auxiliary_loss', auxiliary_loss_sum / sample_size / math.log(2), sample_size, round=3)
+        metrics.log_scalar('primary_nll_loss', primary_nll_loss / ntokens / math.log(2), ntokens, round=3)
+        metrics.log_scalar('auxiliary_nll_loss', auxiliary_nll_loss / ntokens / math.log(2), ntokens, round=3)
+        metrics.log_derived('primary_ppl', lambda meters: utils.get_perplexity(meters['primary_nll_loss'].avg))
+        metrics.log_derived('auxiliary_ppl', lambda meters: utils.get_perplexity(meters['auxiliary_nll_loss'].avg))

examples/speech_recognition/models/conv_transformer_dualdecoder.py
@@ -1,0 +1,94 @@
+from torch import nn
+import torch
+
+from examples.speech_recognition.models.conv_transformer import ConvolutionalTransformerModel, base_architecture, \
+    speechtransformer_big, speechtransformer_big2, ConvolutionalTransformerEncoder
+from examples.speech_recognition.models.multi_task import MultiTaskModel, ClassifierDecoder
+from examples.speech_recognition.tasks.speech_translation_ctc import SpeechTranslationCTCTask
+from fairseq import utils
+from fairseq.models import register_model, register_model_architecture
+from fairseq.models.transformer import TransformerDecoder
+
+
+@register_model('conv_transformer_dualdecoder')
+class ConvolutionalTransformerDualDecoder(MultiTaskModel):
+    """
+    This model is an implementation of a multi-task model that predicts both transcripts
+    and translations, like in (Weiss et al. 2017). It represents the DirMul model of
+    (Sperber et al. 2020).
+    """
+    # TODO: do we need different settings/configs for the two decoders?
+    # For now, we assume NO.
+    @staticmethod
+    def add_args(parser):
+        ConvolutionalTransformerModel.add_args(parser)
+        parser.add_argument('--auxiliary-decoder-embed-path', type=str, metavar='STR',
+                            help='path to pre-trained decoder embedding')
+
+    @classmethod
+    def build_model(cls, args, task):
+        """Build a new model instance."""
+
+        # make sure all arguments are present in older models
+        base_architecture(args)
+
+        if not hasattr(args, 'max_source_positions'):
+            args.max_source_positions = 100000
+        if not hasattr(args, 'max_target_positions'):
+            args.max_target_positions = 100000
+
+        # This model requires a task that provides source dictionary and transcripts
+        assert isinstance(task, SpeechTranslationCTCTask)
+
+        src_dict, tgt_dict = task.source_dictionary, task.target_dictionary
+
+        def build_embedding(dictionary, embed_dim, path=None):
+            num_embeddings = len(dictionary)
+            padding_idx = dictionary.pad()
+            emb = Embedding(num_embeddings, embed_dim, padding_idx)
+            # if provided, load from preloaded dictionaries
+            if path:
+                embed_dict = utils.parse_embedding(path)
+                utils.load_embedding(embed_dict, dictionary, emb)
+            return emb
+
+        target_embed_tokens = build_embedding(
+            tgt_dict, args.decoder_embed_dim, args.decoder_embed_path)
+        src_embed_tokens = build_embedding(
+            src_dict, args.decoder_embed_dim, args.auxiliary_decoder_embed_path)
+        encoder = ConvolutionalTransformerEncoder(
+            args, tgt_dict, audio_features=args.input_feat_per_channel)
+        decoder = TransformerDecoder(args, tgt_dict, target_embed_tokens)
+        auxiliary_decoder = TransformerDecoder(args, src_dict, src_embed_tokens)
+        return ConvolutionalTransformerDualDecoder(encoder, decoder, auxiliary_decoder)
+
+    # In "speech_translation_with_transcription" the transcripts are read into
+    # "encoder_target". Not the most elegant solution, but it allows
+    # compatibility with existing code.
+    def get_auxiliary_target(self, sample, auxiliary_output):
+        return sample["encoder_target"]
+
+
+def Embedding(num_embeddings, embedding_dim, padding_idx):
+    m = nn.Embedding(num_embeddings, embedding_dim, padding_idx=padding_idx)
+    nn.init.normal_(m.weight, mean=0, std=embedding_dim ** -0.5)
+    nn.init.constant_(m.weight[padding_idx], 0)
+    return m
+
+
+@register_model_architecture('conv_transformer_dualdecoder', 'conv_transformer_dualdecoder')
+def base_multilingual_architecture(args):
+    base_architecture(args)
+    args.auxiliary_decoder_embed_path = getattr(args, "auxiliary_decoder_embed_path", None)
+
+
+@register_model_architecture('conv_transformer_dualdecoder', 'conv_transformer_dualdecoder_big')
+def speechtransformer_multilingual_big(args):
+    speechtransformer_big(args)
+    args.auxiliary_decoder_embed_path = getattr(args, "auxiliary_decoder_embed_path", None)
+
+
+@register_model_architecture('conv_transformer_dualdecoder', 'conv_transformer_dualdecoder_big2')
+def speechtransformer_multilingual_big2(args):
+    speechtransformer_big2(args)
+    args.auxiliary_decoder_embed_path = getattr(args, "auxiliary_decoder_embed_path", None)

hlt-mt/FBK-fairseq

examples/speech_recognition/criterions/cross_entropy_dualdecoder.py
@@ -1,0 +1,80 @@
+import math
+
+
+from fairseq import utils, metrics
+from fairseq.criterions import FairseqCriterion, register_criterion
+from fairseq.criterions.label_smoothed_cross_entropy import label_smoothed_nll_loss
+
+
+@register_criterion("cross_entropy_dualdecoder")
+class CrossEntropyDualDecoder(FairseqCriterion):
+    def __init__(self, args, task):
+        super().__init__(task)
+        self.eps = args.label_smoothing
+        self.sentence_avg = args.sentence_avg
+        self.auxiliary_loss_weight = args.auxiliary_loss_weight
+        self.primary_loss_weight = args.primary_loss_weight
+
+    @classmethod
+    def build_criterion(cls, args, task):
+        return cls(args, task)
+
+    @staticmethod
+    def add_args(parser):
+        parser.add_argument('--primary-loss-weight', default=1.0, type=float, metavar='W',
+                            help='The weight to apply to the primary loss function when summing losses')
+        parser.add_argument('--auxiliary-loss-weight', default=1.0, type=float, metavar='W',
+                            help='The weight to apply to the auxiliary loss function when summing losses')
+        parser.add_argument('--label-smoothing', default=0., type=float, metavar='D',
+                            help='epsilon for label smoothing, 0 means no label smoothing')
+
+    def forward(self, model, sample, reduce=True, log_probs=True):
+        net_output = model(**sample['net_input'])
+        sample_size = sample['target'].size(0) if self.sentence_avg else sample['ntokens']
+        lprobs = model.get_normalized_probs(net_output[0], log_probs=True)
+        lprobs = lprobs.view(-1, lprobs.size(-1))
+        target = model.get_targets(sample, net_output[0]).view(-1, 1)
+        primary_loss, primary_nll_loss = label_smoothed_nll_loss(
+            lprobs, target, self.eps, ignore_index=self.padding_idx, reduce=reduce,
+        )
+        auxiliary_probs = model.auxiliary_decoder.get_normalized_probs(net_output[1], log_probs=True)
+        auxiliary_probs = auxiliary_probs.view(-1, auxiliary_probs.size(-1))
+        auxiliary_target = model.get_auxiliary_target(sample, net_output[1]).view(-1, 1)
+        auxiliary_loss, auxiliary_nll_loss = label_smoothed_nll_loss(
+            auxiliary_probs, auxiliary_target, self.eps, ignore_index=self.padding_idx, reduce=reduce,
+        )
+        loss = self.primary_loss_weight * primary_loss + self.auxiliary_loss_weight * auxiliary_loss
+        logging_output = {
+            'loss': loss.data,
+            'primary_loss': primary_loss.data,
+            'primary_nll_loss': primary_nll_loss.data,
+            'auxiliary_loss': auxiliary_loss.data,
+            'auxiliary_nll_loss': auxiliary_nll_loss.data,
+            'ntokens': sample['ntokens'],
+            'nsentences': sample['target'].size(0),
+            'sample_size': sample_size,
+        }
+        return loss, sample_size, logging_output
+
+    @staticmethod
+    def logging_outputs_can_be_summed():
+        return True
+
+    @staticmethod
+    def reduce_metrics(logging_outputs) -> None:
+        """Aggregate logging outputs from data parallel training."""
+        loss_sum = sum(log.get('loss', 0) for log in logging_outputs)
+        primary_loss_sum = sum(log.get('primary_loss', 0) for log in logging_outputs)
+        auxiliary_loss_sum = sum(log.get('auxiliary_loss', 0) for log in logging_outputs)
+        primary_nll_loss = sum(log.get('primary_nll_loss', 0) for log in logging_outputs)
+        auxiliary_nll_loss = sum(log.get('auxiliary_nll_loss', 0) for log in logging_outputs)
+        ntokens = sum(log.get('ntokens', 0) for log in logging_outputs)
+        sample_size = sum(log.get('sample_size', 0) for log in logging_outputs)
+
+        metrics.log_scalar('loss', loss_sum / sample_size / math.log(2), sample_size, round=3)
+        metrics.log_scalar('primary_loss', primary_loss_sum / sample_size / math.log(2), sample_size, round=3)
+        metrics.log_scalar('auxiliary_loss', auxiliary_loss_sum / sample_size / math.log(2), sample_size, round=3)
+        metrics.log_scalar('primary_nll_loss', primary_nll_loss / ntokens / math.log(2), ntokens, round=3)
+        metrics.log_scalar('auxiliary_nll_loss', auxiliary_nll_loss / ntokens / math.log(2), ntokens, round=3)
+        metrics.log_derived('primary_ppl', lambda meters: utils.get_perplexity(meters['primary_nll_loss'].avg))
+        metrics.log_derived('auxiliary_ppl', lambda meters: utils.get_perplexity(meters['auxiliary_nll_loss'].avg))

examples/speech_recognition/models/conv_transformer_dualdecoder.py
@@ -1,0 +1,103 @@
+from torch import nn
+import torch
+
+from examples.speech_recognition.models.conv_transformer import ConvolutionalTransformerModel, base_architecture, \
+    speechtransformer_big, speechtransformer_big2, ConvolutionalTransformerEncoder
+from examples.speech_recognition.models.multi_task import MultiTaskModel, ClassifierDecoder
+from examples.speech_recognition.tasks.speech_translation_ctc import SpeechTranslationCTCTask
+from fairseq import utils
+from fairseq.models import register_model, register_model_architecture
+from fairseq.models.transformer import TransformerDecoder
+
+
+@register_model('conv_transformer_dualdecoder')
+class ConvolutionalTransformerDualDecoder(MultiTaskModel):
+    """
+    This model is an implementation of a multi-task model that predicts both transcripts
+    and translations, like in (Weiss et al. 2017). It represents the DirMul model of
+    (Sperber et al. 2020).
+    """
+    # TODO: do we need different settings/configs for the two decoders?
+    # For now, we assume NO.
+    @staticmethod
+    def add_args(parser):
+        ConvolutionalTransformerModel.add_args(parser)
+        parser.add_argument('--auxiliary-decoder-embed-path', type=str, metavar='STR',
+                            help='path to pre-trained decoder embedding')
+
+    @classmethod
+    def build_model(cls, args, task):
+        """Build a new model instance."""
+
+        # make sure all arguments are present in older models
+        base_architecture(args)
+
+        if not hasattr(args, 'max_source_positions'):
+            args.max_source_positions = 100000
+        if not hasattr(args, 'max_target_positions'):
+            args.max_target_positions = 100000
+
+        # This model requires a task that provides source dictionary and transcripts
+        assert isinstance(task, SpeechTranslationCTCTask)
+
+        src_dict, tgt_dict = task.source_dictionary, task.target_dictionary
+
+        def build_embedding(dictionary, embed_dim, path=None):
+            num_embeddings = len(dictionary)
+            padding_idx = dictionary.pad()
+            emb = Embedding(num_embeddings, embed_dim, padding_idx)
+            # if provided, load from preloaded dictionaries
+            if path:
+                embed_dict = utils.parse_embedding(path)
+                utils.load_embedding(embed_dict, dictionary, emb)
+            return emb
+
+        target_embed_tokens = build_embedding(
+            tgt_dict, args.decoder_embed_dim, args.decoder_embed_path)
+        src_embed_tokens = build_embedding(
+            src_dict, args.decoder_embed_dim, args.auxiliary_decoder_embed_path)
+        encoder = ConvolutionalTransformerEncoder(
+            args, tgt_dict, audio_features=args.input_feat_per_channel)
+        decoder = TransformerDecoder(args, tgt_dict, target_embed_tokens)
+        auxiliary_decoder = TransformerDecoder(args, src_dict, src_embed_tokens)
+        return ConvolutionalTransformerDualDecoder(encoder, decoder, auxiliary_decoder)
+
+    # In "speech_translation_with_transcription" the transcripts are read into
+    # "transcript_target". Not the most elegant solution, but it allows
+    # compatibility with existing code.
+    def get_auxiliary_target(self, sample, auxiliary_output):
+        return sample["transcript_target"]
+
+    def forward(self, src_tokens, src_lengths, prev_output_tokens, transcript_prev_output_tokens, **kwargs):
+        encoder_out = self.encoder(src_tokens, src_lengths=src_lengths, **kwargs)
+        decoder_out = self.decoder(
+            prev_output_tokens, encoder_out=encoder_out, **kwargs
+        )
+        auxiliary_out = self.auxiliary_decoder(
+            transcript_prev_output_tokens, encoder_out=encoder_out, **kwargs)
+        return decoder_out, auxiliary_out
+
+
+def Embedding(num_embeddings, embedding_dim, padding_idx):
+    m = nn.Embedding(num_embeddings, embedding_dim, padding_idx=padding_idx)
+    nn.init.normal_(m.weight, mean=0, std=embedding_dim ** -0.5)
+    nn.init.constant_(m.weight[padding_idx], 0)
+    return m
+
+
+@register_model_architecture('conv_transformer_dualdecoder', 'conv_transformer_dualdecoder')
+def base_multilingual_architecture(args):
+    base_architecture(args)
+    args.auxiliary_decoder_embed_path = getattr(args, "auxiliary_decoder_embed_path", None)
+
+
+@register_model_architecture('conv_transformer_dualdecoder', 'conv_transformer_dualdecoder_big')
+def speechtransformer_multilingual_big(args):
+    speechtransformer_big(args)
+    args.auxiliary_decoder_embed_path = getattr(args, "auxiliary_decoder_embed_path", None)
+
+
+@register_model_architecture('conv_transformer_dualdecoder', 'conv_transformer_dualdecoder_big2')
+def speechtransformer_multilingual_big2(args):
+    speechtransformer_big2(args)
+    args.auxiliary_decoder_embed_path = getattr(args, "auxiliary_decoder_embed_path", None)