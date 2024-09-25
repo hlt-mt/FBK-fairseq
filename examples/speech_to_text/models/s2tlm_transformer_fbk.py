9ecfa1f3ae30555d6d438970b6f4b352ea7025fc
Marco Gaido <mgaido@fbk.eu>
2024-09-25 22:29:29 +0200

    [!229][BUG][ARCH] Fix decoder-only/decoder-prepending implementation
    # Why is the change needed?
    
    The implementation of decoder-only and decoder-prepending introduced in !222 (63d205f709503979d47552a367a1dc281f36b268) does not work properly as it does not support incremental decoding and does not account for the fact that the self-attention mask is cached. The latter point leads to the issue that tokens will be able to look into the future if previously a longer audio has been processed, compromising the training.
    
    # What changes does the patch introduce?
    
    Fixes the self-attention mask handling, also introducing the option to use causal masking in the audio prepended to the decoder. Also, it fixes the incremental decoding process, used in the generate.
    
    # How was this patch tested?
    
    Added UTs

--- examples/speech_to_text/models/s2tlm_transformer_fbk.py
+++ examples/speech_to_text/models/s2tlm_transformer_fbk.py
@@ -17,11 +17,9 @@ from typing import Dict, List, Optional
 import torch
 from torch import Tensor
 
-from fairseq.models import (
-    register_model,
-    register_model_architecture,
-)
-from examples.speech_to_text.models.s2t_transformer_fbk import S2TTransformerModel
+from fairseq.models import register_model, register_model_architecture
+from examples.speech_to_text.models.s2t_transformer_fbk import S2TTransformerModel, \
+    base_architecture, s2t_transformer_s
 from fairseq.models.transformer import TransformerDecoder
 
 
@@ -33,12 +31,8 @@ class S2TLMTransformerModel(S2TTransformerModel):
     @staticmethod
     def add_args(parser):
         S2TTransformerModel.add_args(parser)
-
         parser.add_argument(
-            "--decoder-pos-enc-type", type=str, default="abs"
-        )
-        parser.add_argument(
-            "--mask-prompt", action="store_true", default=False, 
+            "--causal-prompt-mask", action="store_true", default=False,
             help="Apply causual masking on audio prompt"
         )
 
@@ -48,6 +42,10 @@ class S2TLMTransformerModel(S2TTransformerModel):
 
 
 class AudioPrependedTransformerDecoder(TransformerDecoder):
+    def __init__(self, args, task, embed_tokens, no_encoder_attn=False):
+        super().__init__(args, task, embed_tokens, no_encoder_attn)
+        self.prompt_causal_masking = getattr(args, "causal_prompt_mask", False)
+
     def extract_features_scriptable(
             self,
             prev_output_tokens,
@@ -58,23 +56,9 @@ class AudioPrependedTransformerDecoder(TransformerDecoder):
             alignment_heads: Optional[int] = None,
     ):
         """
-        Similar to *forward* but only return features.
-
-        Includes several features from "Jointly Learning to Align and
-        Translate with Transformer Models" (Garg et al., EMNLP 2019).
-
-        Args:
-            full_context_alignment (bool, optional): don't apply
-                auto-regressive mask to self-attention (default: False).
-            alignment_layer (int, optional): return mean alignment over
-                heads at this layer (default: last layer).
-            alignment_heads (int, optional): only average alignment over
-                this many heads (default: all heads).
-
-        Returns:
-            tuple:
-                - the decoder's features of shape `(batch, tgt_len, embed_dim)`
-                - a dictionary with any model-specific outputs
+        Similar to the `TransformerDecoder` forward pass but concatenates the encoder output
+        to the `prev_output_tokens` embeddings, so that the two information interact in the
+        self-attention instead of with the cross-attention mechanism (that is disabled here).
         """
         # embed positions
         positions = (
@@ -112,13 +96,14 @@ class AudioPrependedTransformerDecoder(TransformerDecoder):
 
         self_attn_padding_mask = prev_output_tokens.eq(self.padding_idx)
 
-        if incremental_state is None and not full_context_alignment:
+        if (incremental_state is None or len(incremental_state) == 0) and not full_context_alignment:
             self_attn_mask = self.buffered_future_mask(x)
         else:
             self_attn_mask = None
 
-        # prepending audio to the text tokens only at training time or for the first step of the
-        # generation at inference time, as the prefix is cached for subsequent generations
+        # Prepending encoder output (audio) to the embeddings of the text tokens.
+        # This is done only at training time or in the first step of the generation
+        # at inference time, as the prefix is cached for subsequent generations
         prepended_audio = False
         encoder_out_seq = encoder_out["encoder_out"][0]
         audio_len, batch_size, n_channels_audio = encoder_out_seq.shape
@@ -130,11 +115,17 @@ class AudioPrependedTransformerDecoder(TransformerDecoder):
                 encoder_padding_mask = self_attn_padding_mask.new_zeros(
                     batch_size, audio_len)
             x = torch.cat((encoder_out_seq, x), dim=0)  # T x B x C
+            # update the padding mask to consider the prepended audio
             self_attn_padding_mask = torch.cat(
                 (encoder_padding_mask, self_attn_padding_mask), dim=1)
             if self_attn_mask is not None:
                 self_attn_mask = self.buffered_future_mask(x)
-                self_attn_mask[:audio_len, :audio_len] = 0.
+                if not self.prompt_causal_masking:
+                    # as the future mask is cached, we need to avoid to perform the inplace
+                    # operation (setting to 0) on the cached version, otherwise, in case the next
+                    # batch has a shorter audio, the textual tokens will have access to the future
+                    self_attn_mask = self_attn_mask.clone()
+                    self_attn_mask[:audio_len, :audio_len] = 0.
 
         # decoder layers
         attn: Optional[Tensor] = None
@@ -168,6 +159,8 @@ class AudioPrependedTransformerDecoder(TransformerDecoder):
         x = x.transpose(0, 1)
 
         if prepended_audio:
+            # strip the prepended audio prefix, so we consider only the textual part for coherence
+            # with the output of the cross-attention decoder
             x = x[:, encoder_out_seq.shape[0]:, :]
         if self.project_out_dim is not None:
             x = self.project_out_dim(x)
@@ -176,55 +169,10 @@ class AudioPrependedTransformerDecoder(TransformerDecoder):
 
 
 @register_model_architecture(model_name="s2tlm_transformer_fbk", arch_name="s2tlm_transformer_fbk")
-def base_architecture(args):
-    # Convolutional subsampler
-    args.conv_kernel_sizes = getattr(args, "conv_kernel_sizes", "5,5")
-    args.conv_channels = getattr(args, "conv_channels", 1024)
-    # Transformer
-    args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
-    args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 2048)
-    args.encoder_layers = getattr(args, "encoder_layers", 0)
-    args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 8)
-    args.encoder_normalize_before = getattr(args, "encoder_normalize_before", True)
-    args.decoder_embed_dim = getattr(args, "decoder_embed_dim", args.encoder_embed_dim)
-    args.decoder_ffn_embed_dim = getattr(
-        args, "decoder_ffn_embed_dim", args.encoder_ffn_embed_dim
-    )
-    args.decoder_layers = getattr(args, "decoder_layers", 18)
-    args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 8)
-    args.decoder_normalize_before = getattr(args, "decoder_normalize_before", True)
-    args.decoder_learned_pos = getattr(args, "decoder_learned_pos", False)
-    args.dropout = getattr(args, "dropout", 0.1)
-    args.attention_dropout = getattr(args, "attention_dropout", args.dropout)
-    args.activation_dropout = getattr(args, "activation_dropout", args.dropout)
-    args.activation_fn = getattr(args, "activation_fn", "relu")
-    args.adaptive_softmax_cutoff = getattr(args, "adaptive_softmax_cutoff", None)
-    args.adaptive_softmax_dropout = getattr(args, "adaptive_softmax_dropout", 0)
-    args.share_decoder_input_output_embed = getattr(
-        args, "share_decoder_input_output_embed", False
-    )
-    args.no_token_positional_embeddings = getattr(
-        args, "no_token_positional_embeddings", False
-    )
-    args.adaptive_input = getattr(args, "adaptive_input", False)
-    args.decoder_layerdrop = getattr(args, "decoder_layerdrop", 0.0)
-    args.decoder_output_dim = getattr(
-        args, "decoder_output_dim", args.decoder_embed_dim
-    )
-    args.decoder_input_dim = getattr(args, "decoder_input_dim", args.decoder_embed_dim)
-    args.no_scale_embedding = getattr(args, "no_scale_embedding", False)
-    args.quant_noise_pq = getattr(args, "quant_noise_pq", 0)
+def base_s2tlm_transformer_fbk(args):
+    base_architecture(args)
 
-    args.distance_penalty = getattr(args, 'distance_penalty', False)
-    args.init_variance = getattr(args, 'init_variance', 0.0)
-    args.allow_extra_tokens = getattr(args, 'allow_extra_tokens', False)
 
 @register_model_architecture("s2tlm_transformer_fbk", "s2tlm_transformer_s_fbk")
 def s2tlm_transformer_s(args):
-    args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 256)
-    args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 256 * 8) 
-    args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 4)
-    args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 4)
-    args.dropout = getattr(args, "dropout", 0.1)
-    base_architecture(args)
-
+    s2t_transformer_s(args)
--- fbk_uts/s2tllm/test_stllm.py
+++ fbk_uts/s2tllm/test_stllm.py
@@ -22,6 +22,7 @@ from examples.speech_to_text.models.s2t_transformer_fbk import S2TTransformerEnc
 from examples.speech_to_text.models.s2tlm_transformer_fbk import s2tlm_transformer_s, \
     AudioPrependedTransformerDecoder
 from fairseq.data import Dictionary
+from fairseq.data.data_utils import lengths_to_padding_mask
 from fairseq.models.transformer import Embedding
 
 
@@ -33,7 +34,7 @@ class AudioPrependedTransformerDecoderWrapper(seq2seq.PangolinnSeq2SeqModuleWrap
         args.input_channels = 1
         args.max_source_positions = 10
         args.max_target_positions = 100
-        args.decoder_layers = 2
+        args.decoder_layers = 4
         args.encoder_layers = 0
         args.criterion = "label_smoothed_cross_entropy"
         args.label_smoothing = 0.1
@@ -74,7 +75,48 @@ class S2TLLMTransformerTestCase(seq2seq.CausalTestCase):
     module_wrapper_class = AudioPrependedTransformerDecoderWrapper
 
     def test_gradient_not_flowing_from_future(self):
-        self.assertTrue(True)
+        self.skipTest("Integer is not supported")
+
+    def test_incremental_decoding(self):
+        """
+        Tests that decoding with incremental decoding leads to the same results as doing
+        the forward pass without caching previous steps. We perform three steps to make
+        sure that it does not work only with one previous step cached.
+        """
+        decoder = self.module_wrapper.build_module()
+        x = torch.randint(10, (2, 10))
+        fake_encoder_out = {
+            "encoder_out": [torch.ones(3, x.shape[0], 16)],
+            "encoder_padding_mask": [lengths_to_padding_mask(torch.LongTensor([3, 2]))],
+        }
+        incremental_states = {}
+        pass1_nostates = decoder(x[:, :1], fake_encoder_out)
+        pass1_states = decoder(x[:, :1], fake_encoder_out, incremental_state=incremental_states)
+        torch.testing.assert_close(pass1_nostates[0], pass1_states[0])
+        pass2_nostates = decoder(x[:, :2], fake_encoder_out)
+        pass2_states = decoder(x[:, :2], fake_encoder_out, incremental_state=incremental_states)
+        torch.testing.assert_close(pass2_nostates[0][:, -1:, :], pass2_states[0])
+        pass3_nostates = decoder(x[:, :3], fake_encoder_out)
+        pass3_states = decoder(x[:, :3], fake_encoder_out, incremental_state=incremental_states)
+        torch.testing.assert_close(pass3_nostates[0][:, -1:, :], pass3_states[0])
+
+    def test_buffered_mask(self):
+        decoder = self.module_wrapper.build_module()
+        x = torch.randint(10, (2, 10))
+        fake_encoder_out_shorter = {
+            "encoder_out": [torch.ones(3, x.shape[0], 16)],
+            "encoder_padding_mask": [lengths_to_padding_mask(torch.LongTensor([3, 2]))],
+        }
+        pass1_shorter = decoder(x, fake_encoder_out_shorter)
+        # do 1 pass with longer encoder out so we check that the mask does not remain cached
+        # enabling to look in the future
+        fake_encoder_out_longer = {
+            "encoder_out": [torch.ones(10, x.shape[0], 16)],
+            "encoder_padding_mask": [lengths_to_padding_mask(torch.LongTensor([10, 5]))],
+        }
+        _ = decoder(x, fake_encoder_out_longer)
+        pass2_shorter = decoder(x, fake_encoder_out_shorter)
+        torch.testing.assert_close(pass1_shorter, pass2_shorter)
 
 
 if __name__ == '__main__':
